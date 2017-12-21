37956bc4bd224f18c8ca847afa40710c3cd070ba
Aaron Levick <aaron.levick@zocdoc.com>
2017-12-21 18:12:41 -0500

    Dont hardcode alevick. Move bashrc_local instead of copying so that changes arent persisted on commit

--- link_all_dot_files.sh
+++ link_all_dot_files.sh
@@ -7,6 +7,14 @@ dir=$( cd $(dirname $0) ; pwd -P )
 find $dir/files -type f | 
 while read dot_file; do
     local_file=~/.`basename $dot_file`
-    echo Linking $local_file to $dot_file
-    ln -sf $dot_file $local_file
+    if [[ "$dot_file" = *"bashrc_local"* ]]; then
+        echo Copying $local_file to $dot_file
+        rm $local_file
+        cp $dot_file $local_file
+    else
+        echo Linking $local_file to $dot_file
+        ln -sf $dot_file $local_file
+    fi
 done
+
+touch ~/.vimrc_local
