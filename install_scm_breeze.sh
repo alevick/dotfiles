feb90711881bb8823aad7d0ba21aee4836dd00b7
Aaron Levick <aaron.levick@gmail.com>
2023-03-22 12:34:45 -0400

    Make things more robust

--- install_scm_breeze.sh
+++ install_scm_breeze.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-git clone git://github.com/scmbreeze/scm_breeze.git ~/.scm_breeze
+git clone https://github.com/scmbreeze/scm_breeze.git ~/.scm_breeze
 cd ~/.scm_breeze && ./install.sh
