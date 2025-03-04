fd76eb4dea40ab1b039c2c3125c02bc6eb479409
Aaron Levick <aaron.levick@gmail.com>
2025-03-04 16:54:43 -0500

    A bunch of new gitconfigs and convenience functions

--- install_gnu_utils.sh
+++ install_gnu_utils.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# GNU Grep
 brew install grep
 
 GGREP=`which ggrep`
 LINKED_GGREP=`echo $GGREP | sed 's/ggrep/grep/g'`
 ln -fs $GGREP $LINKED_GGREP
+
+# GNU cut
+# Provides things like the --complement flag
+brew install coreutils
+GCUT=`which gcut`
+ln -fs $GCUT $(echo $GCUT | sed 's/gcut/cut/g')
